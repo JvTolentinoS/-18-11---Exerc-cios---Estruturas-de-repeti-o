1d871fa342bb7af2672dcc5f7daa7ba89685b72d
joao <joaovtolente@gmail.com>
2020-11-18 14:38:28 -0300

    Finalizado + desafio

---
+++ 4 - Sequencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4___Sequencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequência Fibonacci");
+            int n1=0, n2=1, n3, i, numero;
+            numero = 15;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+
+            for(i=2; i<numero;++i)
+            {
+               n3=n1+n2;
+               Console.WriteLine(n3);
+               n1=n2;
+               n2=n3;
+            }
+        }
+    }
+}
+//créditos: https://www.javatpoint.com/fibonacci-series-in-csharp
